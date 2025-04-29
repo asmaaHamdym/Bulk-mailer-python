0b4edb27539162fe8dee6c859ecf9635e9a308d6
asmaaHamdym <asmahamdym@gmail.com>
2025-04-29 17:45:38 +0300

    Basic setup

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import smtplib
 sender = "hamzawanes01@gmail.com"
+receipient = ['asmahamdym@gmail.com', 'engasmamahrous@gmail.com']
+first_name = 'Asma'
+password = 'Semsemah1'
+subject = 'test'
+message = f'Hi, {first_name}'
+
+smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
+smtpObj.login(sender, password)
+smtpObj.sendmail(sender, receipient, message)
